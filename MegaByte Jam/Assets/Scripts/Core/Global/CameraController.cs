00464b6a83a9bdfa8ac769c4ad1de92c8e7d9751
Charlie Sparks <sparksfullstack@gmail.com>
2025-10-22 15:31:35 -0700

    Character Controller | Camera successfully following player

--- MegaByte Jam/Assets/Scripts/Core/Global/CameraController.cs
+++ MegaByte Jam/Assets/Scripts/Core/Global/CameraController.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables and References
+    [Header("External References")]
+    [SerializeField] private PlayerInputReader playerInputReader;
+    [SerializeField] private CinemachineFreeLook freeLookCamera;
+
+    [Header("Camera Settings")]
+    [SerializeField] private float lookSensitivityXAxis = 150f;
+    [SerializeField] private float lookSensitivityYAxis = 2f;
+    [SerializeField] private bool invertY = false;
+
+    private Vector2 cameraInputVector;
+    #endregion
+
+    #region Lifecycle Methods
+    private void OnEnable()
     {
-        
+        playerInputReader.OnControlCameraEvent += HandleControlCameraEvent;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        playerInputReader.OnControlCameraEvent -= HandleControlCameraEvent;
     }
+
+    private void Update()
+    {
+        if (freeLookCamera != null && cameraInputVector != Vector2.zero)
+        {
+            // Horizontal Axis Control
+            freeLookCamera.m_XAxis.Value += cameraInputVector.x * lookSensitivityXAxis * Time.deltaTime;
+
+            // Vertical Axis Control
+            float yInput = invertY ? cameraInputVector.y : -cameraInputVector.y;
+            freeLookCamera.m_YAxis.Value += yInput * lookSensitivityYAxis * Time.deltaTime;
+        }
+    }
+    #endregion
+
+    #region Camera Control Logic
+    private void HandleControlCameraEvent(Vector2 newCameraInputVector)
+    {
+        cameraInputVector = newCameraInputVector;
+    }
+    #endregion
 }
