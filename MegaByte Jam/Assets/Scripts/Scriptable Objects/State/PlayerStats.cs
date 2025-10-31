6e948705181bd0155dcc16b2a7b21dd7aabf9914
Charlie Sparks <sparksfullstack@gmail.com>
2025-10-30 19:54:20 -0700

    Enemy Logic | Added player stats and player script

--- MegaByte Jam/Assets/Scripts/Scriptable Objects/State/PlayerStats.cs
+++ MegaByte Jam/Assets/Scripts/Scriptable Objects/State/PlayerStats.cs
@@ -1,33 +1,70 @@
 using System;
 using UnityEngine;
 
-/// <summary>
-/// This is a template for how all state objects should be written.
-/// To use them, just add a Serialized Field reference in the game object that
-/// needs to reference it and subscribe to the `OnHealthChanged` event.
-/// </summary>
-
 [CreateAssetMenu(fileName = "Player Stats", menuName = "Scriptable Objects/State/New Player Stats")]
 public class PlayerStats : ScriptableObject 
 {
     #region Events
+    public event Action<int> OnDamageTaken;
+    public event Action<int> OnHealed;
+    public event Action OnDeath;
     public event Action OnHealthChanged;
     #endregion
 
     #region Serialized Fields
-    [SerializeField] private int health;
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int attackDamage = 10;
+    [SerializeField] private float attackCooldown = 0.5f;
     #endregion
 
-    #region Getters and Setters
-    public int Health
+    #region Properties
+    public int MaxHealth => maxHealth;
+    public int CurrentHealth { get; private set; }
+    public bool IsAlive => CurrentHealth > 0;
+    public float HealthPercentage => (float)CurrentHealth / maxHealth;
+    public int AttackDamage => attackDamage;
+    public float AttackCooldown => attackCooldown;
+    #endregion
+
+    #region Initialization
+    private void OnEnable()
     {
-        get => health;
-        set
+        ResetHealth();
+    }
+
+    public void ResetHealth()
+    {
+        CurrentHealth = maxHealth;
+    }
+    #endregion
+
+    #region Health Management
+    public void TakeDamage(int damage)
+    {
+        if (!IsAlive || damage <= 0) return;
+
+        int actualDamage = Mathf.Min(damage, CurrentHealth);
+        CurrentHealth -= actualDamage;
+        
+        OnDamageTaken?.Invoke(actualDamage);
+        OnHealthChanged?.Invoke();
+
+        if (CurrentHealth <= 0)
         {
-            if (health == value) return;
-            health = value;
-            OnHealthChanged?.Invoke();
+            CurrentHealth = 0;
+            OnDeath?.Invoke();
         }
+    }
+
+    public void Heal(int amount)
+    {
+        if (!IsAlive || amount <= 0) return;
+
+        int actualHeal = Mathf.Min(amount, maxHealth - CurrentHealth);
+        CurrentHealth += actualHeal;
+        
+        OnHealed?.Invoke(actualHeal);
+        OnHealthChanged?.Invoke();
     }
     #endregion
 }
