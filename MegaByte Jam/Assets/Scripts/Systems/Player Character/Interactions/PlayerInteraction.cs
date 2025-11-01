b9211fe5a0b49021c8091a09d708b991b96a45bd
Charlie Sparks <sparksfullstack@gmail.com>
2025-11-01 17:33:51 -0400

    Fireplace | Added interact to input map

---
+++ MegaByte Jam/Assets/Scripts/Systems/Player Character/Interactions/PlayerInteraction.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] private float interactionRange = 2f;
+    [SerializeField] private LayerMask interactionLayer;
+}
