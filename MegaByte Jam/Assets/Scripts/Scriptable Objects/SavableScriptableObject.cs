2df8b9584a2288b619d5f3c7edc8b0b7e4fba558
Charlie Sparks <sparksfullstack@gmail.com>
2025-11-04 16:31:28 -0500

    Save and Load | Added generic savable scriptable object class for all SOs that need to persist to inherit from

---
+++ MegaByte Jam/Assets/Scripts/Scriptable Objects/SavableScriptableObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Base class for all ScriptableObjects that need to be saved and loaded.
+/// Inherit from this instead of ScriptableObject directly.
+/// </summary>
+public abstract class SavableScriptableObject : ScriptableObject
+{
+    /// <summary>
+    /// Unique key used for saving/loading this ScriptableObject.
+    /// Override this in derived classes to provide a unique identifier.
+    /// </summary>
+    public abstract string SaveKey { get; }
+    
+    /// <summary>
+    /// Called after data is loaded into this ScriptableObject.
+    /// Override this if you need to perform any initialization after loading.
+    /// </summary>
+    public virtual void OnAfterLoad() { }
+}
---
+++ MegaByte Jam/Assets/Scripts/Scriptable Objects/State/PlayerProgression.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Unity;
+using System;
+
+
+/// <summary>
+/// This is an example of how to write a savable piece of state and does not
+/// actually  need to be used at the moment.
+/// </summary>
+[CreateAssetMenu(fileName = "Player Progression",  menuName = "Scriptable Objects/State/Player Progression")]
+public class PlayerProgression : ScriptableObject
+{
+    [SerializeField] private int currentLevel = 1;
+}
