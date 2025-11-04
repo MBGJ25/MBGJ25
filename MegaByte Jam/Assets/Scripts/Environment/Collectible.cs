84b95c22a13fa7fc08741f6e183da4b55a198816
Charlie Sparks <sparksfullstack@gmail.com>
2025-11-04 18:42:04 -0500

    Charlie | Added collectible data so

---
+++ MegaByte Jam/Assets/Scripts/Environment/Collectible.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class Collectible : PlayerInteractableBase
+{
+    
+
+    public override string GetInteractionPrompt()
+    {
+        throw new System.NotImplementedException();
+    }
+    public override PlayerInteractables GetInteractableType()
+    {
+        throw new System.NotImplementedException();
+    }
+    public override bool CanInteract(GameObject player)
+    {
+        return true;
+    }
+    public override void Interact(GameObject player)
+    {
+        // TODO: Add to player inventory
+    }
+}
---
+++ MegaByte Jam/Assets/Scripts/Scriptable Objects/State/CollectibleData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Collectible Data", menuName = "Scriptable Objects/State/New Collectible Data")]
+public class CollectibleData : ScriptableObject
+{
+    #region Collectible Data
+    [Header("Identity")]
+    [SerializeField] private string collectibleID;
+    [SerializeField] private string collectibleName;
+    [TextArea(2, 4)]
+    [SerializeField] private string description;
+    
+    [Header("Visuals")]
+    [SerializeField] private GameObject worldModel;
+    [SerializeField] private Sprite iconSprite;
+    
+    [Header("Audio")]
+    [SerializeField] private AudioClip pickupSound;
+    
+    [Header("VFX (Optional)")]
+    [SerializeField] private ParticleSystem pickupParticles;
+    [SerializeField] private Color highlightColor = Color.yellow;
+    #endregion
+    
+    #region Accessors
+    public string CollectibleID => collectibleID;
+    public string CollectibleName => collectibleName;
+    public string Description => description;
+    public GameObject WorldModel => worldModel;
+    public Sprite IconSprite => iconSprite;
+    public AudioClip PickupSound => pickupSound;
+    public ParticleSystem PickupParticles => pickupParticles;
+    public Color HighlightColor => highlightColor;
+    #endregion
+}
