04c87dfe215001976fe0822d51749f68a05f3bc2
JeriamJito <84357069+JeriamJito@users.noreply.github.com>
2025-11-15 12:10:25 -0800

    Created Healthy Boundaries
    Added healthy boundaries to the game!

--- MegaByte Jam/Assets/Scripts/Player/GetCollectible.cs
+++ MegaByte Jam/Assets/Scripts/Player/GetCollectible.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
 public class GetCollectible : MonoBehaviour
